00774a1a8aebe7d7eeab8f825c1c7da889660152
hgshkt <nichtojestvozxc@gmail.com>
2022-10-19 20:00:01 +0300

    issue #1 is fixed

--- Lab1/Lab1/Game.cs
+++ Lab1/Lab1/Game.cs
@@ -5,10 +5,14 @@ namespace Lab1
     public class Game
     {
         private static Random Random = new Random();
+
+        private static int GamesCount = 0;
         public int Rating { get; }
         public string Name1 { get; }
-        public string Name2{ get; }
+        public string Name2 { get; }
         public string WinnerName { get; }
+
+        public int id { get; }
 
         public Game(string name1, string name2, int rating, string winnerName)
         {
@@ -16,17 +20,28 @@ namespace Lab1
             Name1 = name1;
             Name2 = name2;
             WinnerName = winnerName;
+
+            id = GamesCount;
+            GamesCount++;
         }
 
         public static void imitationGame(GameAccount player1, GameAccount player2, int rating)
         {
             if (Random.Next(0, 2) == 1)
             {
+                Game game = new Game(player1.UserName, player2.UserName, rating, player1.UserName);
+                player1.games.Add(game);
+                player2.games.Add(game);
+
                 player1.WinGame(player2.UserName, rating);
                 player2.LoseGame(player1.UserName, rating);
             }
             else
             {
+                Game game = new Game(player1.UserName, player2.UserName, rating, player2.UserName);
+                player1.games.Add(game);
+                player2.games.Add(game);
+
                 player1.LoseGame(player2.UserName, rating);
                 player2.WinGame(player1.UserName, rating);
             }
--- Lab1/Lab1/GameAccount.cs
+++ Lab1/Lab1/GameAccount.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections;
-using System.Security.AccessControl;
+using System.Collections.Generic;
 
 namespace Lab1
 {
@@ -8,29 +7,25 @@ namespace Lab1
     {
         public string UserName { get; }
         private int CurrentRating = 100, GamesCount = 0;
-        private ArrayList games = new ArrayList();
+        public List<Game> games = new List<Game>();
 
         public GameAccount(string userName)
         {
             UserName = userName;
         }
-
+        
         public void WinGame(string opponentName, int rating)
         {
-            games.Add(new Game(UserName, opponentName, rating, UserName));
             GamesCount++;
-
             CurrentRating += rating;
         }
 
         public void LoseGame(string opponentName, int rating)
         {
-            games.Add(new Game(UserName, opponentName, rating, opponentName));
             GamesCount++;
             if (CurrentRating - rating < 0)
             {
                 CurrentRating = 0;
-                throw new Exception("Rating can't be less than 0");
             }
 
             CurrentRating -= rating;
@@ -41,7 +36,7 @@ namespace Lab1
             Console.WriteLine("\nPlayer name : " + UserName + "\nCurrent rating : " + CurrentRating + "\nGame list :");
             foreach (Game game in games)
             {
-                Console.WriteLine("\nFirst player name : " + game.Name1 + "\nSecond player name : " + game.Name2
+                Console.WriteLine("\nGame id: " + game.id + "\nFirst player name : " + game.Name1 + "\nSecond player name : " + game.Name2
                                   + "\nRating : " + game.Rating + "\nWinner : " + game.WinnerName);
             }
         }
