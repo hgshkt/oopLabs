3c3a7b980a1b18ae65d94dfcd4b15442055840bb
hgshkt <nichtojestvozxc@gmail.com>
2022-11-27 17:38:13 +0300

    InternetShop MainWindow

---
+++ InternetShop/InternetShop/CurrentUser.cs
@@ -0,0 +1,13 @@
+﻿namespace InternetShop;
+
+public static class CurrentUser
+{
+    private static string name;
+
+    public static string Name
+    {
+        get => name;
+        set => name = value;
+    }
+
+}
---
+++ InternetShop/InternetShop/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace InternetShop
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void login(object sender, RoutedEventArgs e)
+        {
+            CurrentUser.Name = TextBox.Text;
+        }
+    }
+}
